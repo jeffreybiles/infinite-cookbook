65cf10dffc007886c09fa270d7a9afa34bb36a4c
Jeffrey Biles <jeffrey.biles@swimtopia.com>
2025-01-04 12:14:20 -0600

    link to preferences page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import Scraper from "@/components/Scraper";
 import Generator from "../components/Generator";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="items-center justify-items-center p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-4xl font-bold mb-0 pb-0">Infinite Cookbook</h1>
-      <p className="text-lg mt-0 pt-0">As many recipes as you want, completely customizable</p>
+      <p className="text-lg m-0 p-0">As many recipes as you want, completely customizable (<Link href="/preferences" className="text-lg mt-0 pt-0 text-blue-500">update preferences now</Link>)</p>
+
       <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-4">
         <div>
           <h2 className="text-2xl font-bold text-left w-full">Customize an existing recipe</h2>
