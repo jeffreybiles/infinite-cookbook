a4b31b6db2ab43296934d9740408775b4a458693
Jeffrey Biles <jeffrey.biles@swimtopia.com>
2025-02-22 08:07:36 -0600

    apply any further instructions

--- backend/workflows/scrape_url.py
+++ backend/workflows/scrape_url.py
@@ -1,7 +1,6 @@
-from db.recipe import Recipe, add_to_db, update_recipe as update_recipe_in_db
 from prompts import preferences_prompt
 from my_types import Preferences
-from ai_helpers import completion, generate_name
+from ai_helpers import completion, json_completion
 from fastapi import HTTPException
 from exa_py import Exa
 import os
@@ -14,8 +13,35 @@ exa = Exa(os.getenv("EXA_API_KEY"))
 async def scrape_url_and_save(url: str, preferences: Preferences, prompt: str):
   recipe_completion = scrape_and_extract(url)
   recipe_with_preferences = update_with_preferences(url, preferences, recipe_completion)
-  db_recipe = await save_to_db(recipe_with_preferences, prompt)
+  if has_further_instructions(prompt):
+    recipe_with_further_instructions = completion(f"""
+      Apply the following instructions to a recipe.  The recipe was found from the URL in the prompt ({url}).
+
+      Remember to include the URL as a the source, but say that you modified it to fit my saved preferences and custom instructions.
+
+      Instructions: {prompt}
+
+      Recipe: {recipe_with_preferences}
+    """)
+    print("recipe_with_further_instructions", recipe_with_further_instructions)
+    db_recipe = await save_to_db(recipe_with_further_instructions, prompt)
+  else:
+    print("recipe with no further instructions", recipe_with_preferences)
+    db_recipe = await save_to_db(recipe_with_preferences, prompt)
   return db_recipe
+
+def has_further_instructions(prompt: str):
+  result = json_completion(f"""
+    I want to check if the following prompt requests any modifications to the recipe found at the URL.
+
+    {prompt}
+
+    Return json with the following key:
+    - has_further_instructions: boolean
+  """)
+  print("has_further_instructions", result)
+  return result["has_further_instructions"]
+
 
 def update_with_preferences(url: str, preferences: Preferences, recipe_completion: str):
   return completion(f"""
